d430f5637d227e4d1eb8fd3f2efa948cea6e4528
Aaron Kromer <aaron.kromer@gmail.com>
2012-04-10 08:52:19 -0400

    Iteration 1: Complete. Tweets over 140 characters are rejected.

--- jstwitter.rb
+++ jstwitter.rb
@@ -9,6 +9,10 @@ class JSTwitter
   end
 
   def tweet message
-    @client.update message
+    if message.length > 140
+      puts "Warning: Tweet is over 140 characters. Tweet rejected."
+    else
+      @client.update message
+    end
   end
 end
